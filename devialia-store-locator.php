45d1a548d4c9898a0af73bc7f61fce62d0982f14
root <miguel@devialia.com>
2018-03-19 17:51:19 +0100

    Arreglado el comprobador de que está activado el plugin de ACF PRO

--- devialia-store-locator.php
+++ devialia-store-locator.php
@@ -15,7 +15,7 @@
 
 register_activation_hook( __FILE__, 'check_acf_plugin_installed' );
 function check_acf_plugin_installed(){
-    if ( !is_plugin_active( 'advanced-custom-fields-pro/advanced-custom-fields-pro.php' ) ) {
+    if ( !is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
            // Para las máquinas y muestra un error
            wp_die('Uppsss. Este plugin necesita que esté activado el plugin 
                    "Advanced Custom Fields" por lo que no se puede activar.. <br>
